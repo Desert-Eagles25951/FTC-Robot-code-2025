8b34f1e2c2bb9704ba09b1ad2fca60bcdede6735
edy <117540486+itsedy20@users.noreply.github.com>
2024-11-23 23:14:07 -0600

    v4

--- FtcRobotController-master (1)/FtcRobotController-master/TeamCode/src/main/java/robot.java
+++ FtcRobotController-master (1)/FtcRobotController-master/TeamCode/src/main/java/robot.java
@@ -15,11 +15,10 @@ public class robot extends CommandOpMode
 {
 
     private MotorEx right,left;
-    private double wheel_diameter = 4;
+    private final  double wheel_diameter = 4;
 
-    private powertrain drivetrain = new powertrain(right,left,wheel_diameter);
-    private GamepadEx ps4 = new GamepadEx(gamepad1);
-    private drive drivecomand;
+    private final powertrain drivetrain = new powertrain(right,left,wheel_diameter);
+    private final GamepadEx ps4 = new GamepadEx(gamepad1);
 
 
     @Override
@@ -27,7 +26,10 @@ public class robot extends CommandOpMode
       right = new MotorEx(hardwareMap, "right");
       left = new MotorEx(hardwareMap, "left");
 
-      drivecomand = new drive(drivetrain, ()-> ps4.getLeftY(), ()-> ps4.getRightX());
+        drive drivecomand = new drive(drivetrain
+                , ps4::getLeftY
+                , ps4::getRightX
+        );
 
       drivetrain.setDefaultCommand(drivecomand);
 
