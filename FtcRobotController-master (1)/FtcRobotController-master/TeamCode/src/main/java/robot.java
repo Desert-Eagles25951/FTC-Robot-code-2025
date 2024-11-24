3f286bb8011a051a43ae35cb81a8462d5a71cbae
edy <117540486+itsedy20@users.noreply.github.com>
2024-11-23 23:21:51 -0600

    a

--- FtcRobotController-master (1)/FtcRobotController-master/TeamCode/src/main/java/robot.java
+++ FtcRobotController-master (1)/FtcRobotController-master/TeamCode/src/main/java/robot.java
@@ -1,9 +1,6 @@
 import com.arcrobotics.ftclib.command.CommandOpMode;
 import com.arcrobotics.ftclib.gamepad.GamepadEx;
-import com.arcrobotics.ftclib.hardware.motors.Motor;
 import com.arcrobotics.ftclib.hardware.motors.MotorEx;
-import com.qualcomm.hardware.motors.RevRobotics20HdHexMotor;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.drive;
@@ -17,20 +14,21 @@ public class robot extends CommandOpMode
     private MotorEx right,left;
     private final  double wheel_diameter = 4;
 
-    private final powertrain drivetrain = new powertrain(right,left,wheel_diameter);
+    private  powertrain drivetrain;
     private final GamepadEx ps4 = new GamepadEx(gamepad1);
+    private drive drivecomand;
 
 
     @Override
     public void initialize() {
-      right = new MotorEx(hardwareMap, "right");
-      left = new MotorEx(hardwareMap, "left");
+      drivetrain = new powertrain(hardwareMap, "left","right",wheel_diameter);
 
-        drive drivecomand = new drive(drivetrain
+         drivecomand = new drive(drivetrain
                 , ps4::getLeftY
                 , ps4::getRightX
         );
 
+      register(drivetrain);
       drivetrain.setDefaultCommand(drivecomand);
 
 
