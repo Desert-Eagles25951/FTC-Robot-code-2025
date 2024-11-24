e25f8e32fed30add28b3c5076aa06e4b046e68f0
edy <117540486+itsedy20@users.noreply.github.com>
2024-11-23 23:01:21 -0600

    code test v2

--- FtcRobotController-master (1)/FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/powertrain.java
+++ FtcRobotController-master (1)/FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/powertrain.java
@@ -6,27 +6,29 @@ import com.arcrobotics.ftclib.command.Command;
 import com.arcrobotics.ftclib.command.SubsystemBase;
 import com.arcrobotics.ftclib.drivebase.DifferentialDrive;
 import com.arcrobotics.ftclib.hardware.motors.Motor;
+import com.arcrobotics.ftclib.hardware.motors.MotorEx;
+import com.arcrobotics.ftclib.hardware.motors.Motor.Encoder;
+
 import com.qualcomm.hardware.motors.RevRobotics20HdHexMotor;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
+import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.Range;
 
 public class powertrain extends SubsystemBase {
 
-    private final Motor left = new Motor(hardwareMap, "left");
-    private final Motor right = new Motor(hardwareMap, "right");
 
-    private final DifferentialDrive drive = new DifferentialDrive(left,right);
+    private final DifferentialDrive drive;
+    private double Wheel_diamenter;
 
-    //double PosRight;
-    //double PosLeft;
+    private final Encoder leftencoder, rightencoder;
 
-
-    public powertrain() {
+    //config
+    public powertrain(MotorEx right, MotorEx left, final double diameter) {
 
        left.setRunMode(Motor.RunMode.VelocityControl);
        right.setRunMode(Motor.RunMode.VelocityControl);
@@ -40,16 +42,51 @@ public class powertrain extends SubsystemBase {
        left.resetEncoder();
        right.resetEncoder();
 
-       //PosLeft = left.getPositionCoefficient();
-       //PosRight = right.getPositionCoefficient();
+       leftencoder = left.encoder;
+       rightencoder = right.encoder;
+
+
+        drive =  new DifferentialDrive(left,right);
+        Wheel_diamenter = diameter;
 
     }
 
+    //clas for motor name
+    public powertrain(HardwareMap hMap, final String leftMotorName, String rightMotorName,
+                      final double diameter) {
+        this(new MotorEx(hMap, leftMotorName), new MotorEx(hMap, rightMotorName), diameter);
+    }
 
-
+    //arcade drive
     public void arcade(double speed, double turn){
 
         drive.arcadeDrive(speed,turn);
+    }
+
+
+    public double getLeftEncoderVal() {
+        return leftencoder.getPosition();
+    }
+
+    public double getLeftEncoderDistance() {
+        return leftencoder.getRevolutions() * Wheel_diamenter * Math.PI;
+    }
+
+    public double getRightEncoderVal() {
+        return rightencoder.getPosition();
+    }
+
+    public double getRightEncoderDistance() {
+        return rightencoder.getRevolutions() * Wheel_diamenter * Math.PI;
+    }
+
+    public void resetEncoders() {
+        leftencoder.reset();
+        rightencoder.reset();
+    }
+
+    public double getAverageEncoderDistance() {
+        return (getLeftEncoderDistance() + getRightEncoderDistance()) / 2.0;
     }
 
     @Override
--- FtcRobotController-master (1)/FtcRobotController-master/TeamCode/src/main/java/robot.java
+++ FtcRobotController-master (1)/FtcRobotController-master/TeamCode/src/main/java/robot.java
@@ -1,43 +1,39 @@
+import com.arcrobotics.ftclib.command.CommandOpMode;
 import com.arcrobotics.ftclib.gamepad.GamepadEx;
 import com.arcrobotics.ftclib.hardware.motors.Motor;
+import com.arcrobotics.ftclib.hardware.motors.MotorEx;
 import com.qualcomm.hardware.motors.RevRobotics20HdHexMotor;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
-
-import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.util.Range;
 
 import org.firstinspires.ftc.teamcode.drive;
 import org.firstinspires.ftc.teamcode.powertrain;
 
-import java.util.function.DoubleSupplier;
-
 
 @TeleOp(name="drive", group="drive.v2")
-public class robot extends OpMode
+public class robot extends CommandOpMode
 {
-   private final powertrain drivetrain = new powertrain();
 
-    public GamepadEx ps4 = new GamepadEx(gamepad1);
+    private MotorEx right,left;
+    private double wheel_diameter = 4;
 
-
-    @Override
-    public void init() {
-
-    }
+    private powertrain drivetrain = new powertrain(right,left,wheel_diameter);
+    private GamepadEx ps4 = new GamepadEx(gamepad1);
+    private drive drivecomand;
 
 
     @Override
-    public void loop() {
-     drivetrain.setDefaultCommand(new drive(
-             drivetrain,
-             ()-> ps4.getLeftY(),
-             ()-> ps4.getRightX()
-     ));
+    public void initialize() {
+      right = new MotorEx(hardwareMap, "right");
+      left = new MotorEx(hardwareMap, "left");
+
+      drivecomand = new drive(drivetrain, ()-> ps4.getLeftY(), ()-> ps4.getRightX());
+
+      drivetrain.setDefaultCommand(drivecomand);
+
 
     }
+
 
 }
 
